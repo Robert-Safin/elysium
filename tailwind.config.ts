e2f402da1eac31cc046d8f7eb83c3dfa38301e37
Robert Safin <robertsafin98@gmail.com>
2023-09-20 16:32:14 +0800

    assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,23 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        themeRed: "#FF1B1B",
+        themePurple: "#C082FF",
+        themeYellow: "#EDF738",
+        themeBlue: "#3E6DD9",
+        themeOrange: "#FF8A00",
+        themeGray: "#E8E8E8",
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
